d585c2991d7777af9ba31bad3768b13299b39953
alankarapte <alankar@vedirasolutions.com>
2021-05-01 01:00:53 +0530

    Added validation logic

--- js/script.js
+++ js/script.js
@@ -5,8 +5,28 @@ let bttnExpenseAdd = document.querySelector("#bttn-expense-add");
 
 bttnExpenseClear.addEventListener("click", () => {
     console.log("clear clicked");
+    inputExpenseReason.value = "";
+    inputExpenseAmount.value = "";
 });
 
 bttnExpenseAdd.addEventListener("click", () => {
     console.log("add clicked");
+    const reason = inputExpenseReason.value;
+    const amount = inputExpenseAmount.value;
+
+    let isValid = true;
+
+    if (reason.trim().length <= 0) {
+        console.warn("invalid reason");
+        isValid = false;
+    }
+
+    if (amount.trim().length <= 0 || amount <= 0) {
+        console.warn("invalid amount");
+        isValid = false;
+    }
+
+    if (isValid) {
+        console.log("reason: " + reason + ", amount: " + amount);
+    }
 });
