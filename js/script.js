08655fd816af0bc2b681a3908d691d5351651b87
alankarapte <alankar@vedirasolutions.com>
2021-05-01 00:28:56 +0530

    Added ids & event listeners

---
+++ js/script.js
@@ -0,0 +1,12 @@
+let inputExpenseReason = document.querySelector("#input-expense-reason");
+let inputExpenseAmount = document.querySelector("#input-expense-amount");
+let bttnExpenseClear = document.querySelector("#bttn-expense-clear");
+let bttnExpenseAdd = document.querySelector("#bttn-expense-add");
+
+bttnExpenseClear.addEventListener("click", () => {
+    console.log("clear clicked");
+});
+
+bttnExpenseAdd.addEventListener("click", () => {
+    console.log("add clicked");
+});
